b70aa4854bd45a12927dfc3794827b736e67a980
Hy-LeTuan <letuanhy1507@gmail.com>
2025-07-05 14:03:57 -0500

    update config

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -1,21 +1,11 @@
--- return {
--- 	"Mofiqul/vscode.nvim",
--- }
-
--- return { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
---
--- return {
--- 	"rose-pine/neovim",
--- 	name = "rose-pine",
--- 	config = function()
--- 		vim.cmd("colorscheme rose-pine")
--- 	end,
--- }
-
 return {
-	"rebelot/kanagawa.nvim",
-	name = "kanagawa",
+	"navarasu/onedark.nvim",
+	priority = 1000, -- make sure to load this before all the other start plugins
 	config = function()
-		vim.cmd("colorscheme kanagawa")
+		require("onedark").setup({
+			style = "darker",
+		})
+		-- Enable theme
+		require("onedark").load()
 	end,
 }
