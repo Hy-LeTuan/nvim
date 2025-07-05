a0a9000f63956c243b6c7d57e12c216db7024e27
Hy-LeTuan <letuanhy1507@gmail.com>
2025-07-05 14:33:33 -0500

    update color scheme

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -1,11 +1,1 @@
-return {
-	"navarasu/onedark.nvim",
-	priority = 1000, -- make sure to load this before all the other start plugins
-	config = function()
-		require("onedark").setup({
-			style = "darker",
-		})
-		-- Enable theme
-		require("onedark").load()
-	end,
-}
+return { 'projekt0n/github-nvim-theme', name = 'github-theme' }
